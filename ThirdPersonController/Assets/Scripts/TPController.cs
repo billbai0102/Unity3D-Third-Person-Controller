14e03e87907f6e57a0552acb2aad7ad5ec038206
Bill Bai <billbai0102@gmail.com>
2019-11-14 19:36:50 -0500

    Basic rigidbody controls added

---
+++ ThirdPersonController/Assets/Scripts/TPController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPController : MonoBehaviour
+{
+    public Rigidbody person;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        person = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            person.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            person.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            person.AddForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            person.AddForce(Vector3.right * speed);
+        }
+    }
+}
